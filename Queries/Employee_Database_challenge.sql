e84f2d232206bbf7eabd7b3c36672547de3bba6f
Ramón Alonso <77303185+ramon0101alonso@users.noreply.github.com>
2022-08-04 16:52:44 -0400

    Module 7

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -48,3 +48,19 @@ INNER JOIN titles
 ON dept_emp.emp_no = titles.emp_no
 WHERE (dept_emp.to_date = '9999-01-01') AND (employees.birth_date  BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY  employees.emp_no;
+
+SELECT DISTINCT on (employees.emp_no) employees.emp_no, 
+employees.first_name, 
+employees.last_name,
+employees.birth_date, 
+dept_emp.from_date, 
+dept_emp.to_date,
+titles.title
+INTO mentorship_eligibilitythreeyears
+FROM employees 
+INNER JOIN dept_emp
+ON dept_emp.emp_no = employees.emp_no
+INNER JOIN titles
+ON dept_emp.emp_no = titles.emp_no
+WHERE (dept_emp.to_date = '9999-01-01') AND (employees.birth_date  BETWEEN '1960-01-01' AND '1965-12-31')
+ORDER BY  employees.emp_no;
